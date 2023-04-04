b325f4a5e3983110c41c6cba032f8d51950fe04f
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2023-04-04 16:09:19 +0530

    Time: 19 ms (95.38%), Space: 10.3 MB (77.04%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
+    // Nice bitwise method to not use set, 0th position bit will represent whether a is present or not 
+    bool checkBit(int bits, int n){
+        return (bits & (1 << n));
+    }
+    void setBit(int& bits, int n){ 
+        bits |= (1 << n);  
+    }
+
     int partitionString(string s) {
-        unordered_set<char> st;
-        int res = 1;
+        int res = 1, bits = 0;
         for(auto c : s){
-            if(st.find(c) != st.end()){
-                st.clear();
+            int n = c - 'a';
+            if(checkBit(bits, n)){ 
+                bits = 0;
                 res++;
             }
-            st.insert(c);
+            setBit(bits, n); 
         }
         return res;
     }
