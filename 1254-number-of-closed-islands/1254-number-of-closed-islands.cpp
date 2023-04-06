8979e7e05ec9c9efad7b831dfd3b0b8d1e8ef7dc
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2023-04-07 01:23:50 +0530

    Time: 15 ms (52.38%), Space: 13.2 MB (17.44%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -10,7 +10,7 @@ public:
         
         for(int i = 1; i < n-1; i++){
             for(int j = 1; j < m-1; j++){
-                if(grid[i][j] == 0 && bfs(i, j, grid))  res++, cout<<i<<" "<<j<<endl;
+                if(grid[i][j] == 0 && bfs(i, j, grid))  res++;
             }
         }
         return res;
@@ -20,8 +20,8 @@ public:
         bool res = true;
         queue<pair<int, int>> q;
         q.push({i, j});
-        grid[i][j] = -1;             // using -1 to mark cells visited
-
+        grid[i][j] = -1;             
+        
         while(!q.empty()){
             auto cell = q.front();   q.pop();
             
@@ -29,7 +29,7 @@ public:
                 int x = cell.first + dx[k];
                 int y = cell.second + dy[k];
 
-                if(x < 0 || y < 0 || x == n || y == m)   res = false;  // directly returning false here, resulted in a WA where the not really closed island was closed by -1, this would be avoided if we allow bfs to run untill all adjacent cells containing are visited
+                if(x < 0 || y < 0 || x == n || y == m)   res = false;  
                 else if(grid[x][y] == 0){  
                     grid[x][y] = -1;
                     q.push({x, y});
