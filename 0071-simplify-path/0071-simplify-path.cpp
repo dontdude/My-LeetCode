019bd1ea2fd1f019cd4cfda370d3731fd8c6d930
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2023-04-12 23:04:09 +0530

    Time: 3 ms (92.26%), Space: 9.4 MB (70.22%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        int n = path.size(); 
         vector<string> filePaths;
+        int start = -1;
+        for(int end = 0; end < path.size(); end++){
+            if(start == -1 && path[end] != '/')  start = end;
 
-        for(int i = 0; i < n; i++){
-            string directory = "";
-            while(i < n && path[i] != '/'){
-                directory += path[i++];
+            else if(path[end] == '/' && start != -1){  
+               string curr = path.substr(start, end - start);
+
+               if(curr == ".." && filePaths.size() != 0)  filePaths.pop_back();
+               else if(curr != ".." && curr != "." && curr != "")  filePaths.push_back(curr);
+               start = -1;
             }
+        }
+        if(start != -1) {
+            string curr = path.substr(start);
 
-            if(directory == "." || directory == "")  continue;
-            if(directory == ".." && filePaths.size() != 0)  filePaths.pop_back();
-            else if(directory != "..") filePaths.push_back(directory);
+            if(curr == ".." && filePaths.size() != 0)  filePaths.pop_back();
+            else if(curr != ".." && curr != "." && curr != "")  filePaths.push_back(curr);
         }
         
         if(filePaths.size() == 0)   return "/";
