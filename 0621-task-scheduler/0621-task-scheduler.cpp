88e696893e57453db9ae534522618f6e97eb2230
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2023-06-13 13:54:38 +0530

    Time: 84 ms (56.85%), Space: 34.4 MB (84.43%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
+        // for the case, when no of unique elements > n+1, in that case output is task.size(), 
+        // therefore no need to create those groups of task individually,
+        // just use the count max occ logic, you used in previous solution
+
         unordered_map<char, int> mp;
-        for(int i = 0; i < tasks.size(); i++)  mp[tasks[i]]++;
+        int mx = 0;
 
-        priority_queue<pair<int, char>> pq;
-        for(auto m : mp)  pq.push({m.second, m.first});
-
-        int res = 0;
-        while(pq.size() >= n+1){
-            vector<pair<int, char>> temp(n+1);
-            for(int i = 0; i < n+1; i++){
-                auto curr = pq.top();   pq.pop();
-                temp[i] = {curr.first - 1, curr.second};
-            }
-            for(int i = 0; i < n+1; i++){
-                if(temp[i].first)  pq.push(temp[i]);
-            }
-            res += n+1;
+        for(int i = 0; i < tasks.size(); i++){
+            mp[tasks[i]]++;
+            mx = max(mx, mp[tasks[i]]);
         }
+
+        int times = 0;
+        for(auto m : mp)  if(m.second == mx)  times++;
         
-        int mx = -1, times = 0;
-        while(!pq.empty() && (mx == -1 ||  mx == pq.top().first)){ 
-            mx = pq.top().first;  pq.pop();  
-            times++;
-        }
-        if(times)  res += (mx - 1) * (n + 1) + times;
+        int res = (mx - 1) * (n + 1) + times;
 
-        return res;
+        return max(res, (int)tasks.size());
     }
 };
