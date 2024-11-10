debaabc0e61792097221302e41543cdffb33c801
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2024-11-11 00:12:38 +0530

    Time: 550 ms (16.48%), Space: 236 MB (24.01%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,35 +1,49 @@
 class Solution {
 public:
-    int binarySearchForLessThanHalf(vector<int>& curr, int target, bool isHalfCase) {
-        int b = 0, e = curr.size() - 1;
-        while(b <= e) {
-            int m = b + (e - b) / 2;
-            long long current = curr[m];
-            if(isHalfCase) {
-                current *= 2LL;
-            }
-            if(current < target) {
-                b = m + 1;
+     int mergeWithReversePairCount(vector<int>& nums, int b, int m, int e) {
+        int count = 0;
+        for(int i = b, j = m + 1; i <= m; i++) {
+           while(j <= e && nums[i] > 2LL * nums[j]) {
+            j++;
+           }
+           count += j - (m + 1);
+        }
+
+        int i = b, j = m + 1;
+        vector<int> temp;
+        while(i <= m && j <= e) {
+            if(nums[i] <= nums[j]) {
+                temp.push_back(nums[i++]);
             } else {
-                e = m - 1;
+                temp.push_back(nums[j++]);
             }
         }
 
-        return e;
+        while(i <= m) {
+            temp.push_back(nums[i++]);
+        }
+        while(j <= e) {
+            temp.push_back(nums[j++]);
+        }
+
+        for(int k = 0; k < temp.size(); k++) {
+            nums[k + b] = temp[k];
+        }
+
+        return count;
+    }
+    
+    int mergeSort(vector<int>& nums, int b, int e) {
+        int m = b + (e - b) / 2, res = 0;
+        if(b >= e)  return res;
+
+        res += mergeSort(nums, b, m);
+        res += mergeSort(nums, m + 1, e);
+        res += mergeWithReversePairCount(nums, b, m, e);
+        return res;
     }
 
     int reversePairs(vector<int>& nums) {
-        int n = nums.size(), res = 0;
-        vector<int> sortedFromBehind;
-        sortedFromBehind.push_back(nums[n - 1]);
-
-        for(int i = n - 2; i >= 0; i--) {
-            int count = binarySearchForLessThanHalf(sortedFromBehind, nums[i], true);
-            int pos = binarySearchForLessThanHalf(sortedFromBehind, nums[i], false);
-            res +=  count + 1;
-            sortedFromBehind.insert(sortedFromBehind.begin() + pos + 1, nums[i]);
-        }
-
-        return res;
+       return mergeSort(nums, 0, nums.size() - 1);
     }
 };
