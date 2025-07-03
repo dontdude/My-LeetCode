1f033fb60ce55e501865328c8996a3b0bbeef065
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2025-07-03 23:19:16 +0530

    Time: 0 ms (100%), Space: 11 MB (12.44%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,13 +11,35 @@
  */
 class Solution {
 public:
-    vector<int> res;
-    vector<int> inorderTraversal(TreeNode* root) {
-        if(root == NULL) return res;
+    // Recursive Solution:
+    // vector<int> res;
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     if(root == NULL) return res;
 
-        inorderTraversal(root->left);
-        res.push_back(root->val);
-        inorderTraversal(root->right);
+    //     inorderTraversal(root->left);   // traverse all the node in left, by breaking the control flow, to reach last
+    //     res.push_back(root->val);        
+    //     inorderTraversal(root->right);  // traverse all the node in right, by breaking the control flow
+
+    //     return res;
+    // }
+
+    // Iterative Solution:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+
+        while(curr || !st.empty()) {
+            while(curr) {               
+                st.push(curr);
+                curr = curr->left;      // traverse all the node in left, loop here breaks the control flow, to reach last
+            }
+
+            curr = st.top();   st.pop();
+            res.push_back(curr->val);
+
+            curr = curr->right;          // traverse all the node of right, auto breaks the control flow.. since last line
+        }
 
         return res;
     }
