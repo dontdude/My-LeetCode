1b4dcdb92d75dcc58cd7df59936801fbdaf7dd09
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2025-12-15 23:02:34 +0530

    Difficulty: Medium, Time: (95 ms (25.44%)), Space: (88.9 MB (39.14%))

--- LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         int res = 0;
-        unordered_set<int> hashSet(nums.begin(), nums.end());
+        unordered_set<int> s(nums.begin(), nums.end());
 
-        for(int num : hashSet) {  //removes duplicate
+        for(int num : s) {
+            if(s.find(num - 1) == s.end()) {
+                int curr = 1;
+                int n = num;
 
-            // Optimization: only traverse a group nexts (num + 1), if it is the first one (that is no num - 1 of this exsist)
-            if(hashSet.find(num - 1) == hashSet.end()) {
-                int curr_streak = 1;
-                int curr_num = num;
-                
-                while(hashSet.find(curr_num + 1) != hashSet.end()) {    
-                    curr_streak++;
-                    curr_num = curr_num + 1;
+                while(s.find(n + 1) != s.end()) {
+                    curr++;
+                    n++;
                 }
 
-                res = max(res, curr_streak);
+                res = max(res, curr);
             }
         }
 
