c5773dcf8ac218124f8daa11d1b19a45bac23803
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2025-12-15 23:23:53 +0530

    Difficulty: Hard, Time: (N/A (0%)), Space: (N/A (0%))

--- LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.cpp
+++ LeetCode/Hard/0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        // we will negate the value at that index, to mark that index vis
-        // before this, we will be changing the negative integers to 0, so they don't interfere
-        // if a value at a index is already 0, then to negate we will use -(size of nums + 1)
-        // so any negative value.. will basically tell his index is a valid number already in array
-
-        int size = nums.size(), i = 0;
-
-        for(i = 0; i < size; i++) {
-            if(nums[i] < 0)  nums[i] = 0;   // remove every negative value.. with zero
-        }
+        int n = nums.size();
         
-        for(i = 0; i < size; i++) {
-            int num_index = abs(nums[i]) - 1;
-            if(num_index >= 0 && num_index < size) {
-                if(nums[num_index] == 0) {   // to make the index presence.. make that zero stored at num index.. such a large negative value.. that we never process it's index. 
-                    nums[num_index] = -1 * (size + 1);
-                } else if(nums[num_index] > 0) {  // only negate if already not negated, to mark presence
-                    nums[num_index] *= -1;
-                }
+        for(int &  num : nums) {
+            if(num < 0)  num = 0;
+        }
+
+        for(int i = 0; i < n; i++) {
+            int ind = abs(nums[i]) - 1;
+            if(ind >= n || ind < 0)  continue;
+
+            if(nums[ind] == 0) {
+                nums[ind] = -1 * (n + 1);
+            } else {
+                nums[ind] *= -1;
             }
         }
 
-
-        for(i = 0; i < size; i++) {
-            if(nums[i] >= 0)  return i + 1;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] >= 0) {
+                return i + 1;
+            }
         }
 
-        return i + 1;
+        return n + 1;
     }
 };
