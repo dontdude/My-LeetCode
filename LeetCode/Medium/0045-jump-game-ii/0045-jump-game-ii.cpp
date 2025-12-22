ff08c0017207a18be875a59f6896aed78f2cf828
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2025-12-22 10:50:50 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (20.5 MB (61.47%))

--- LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.cpp
+++ LeetCode/Medium/0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        if(n <= 1)  return 0;
+        if(n == 1)  return 0;
 
+        int min_jumps = 0;
         int current_end = 0, farthest = 0;
-        int jumps = 0;
 
         for(int i = 0; i < n; i++) {
             farthest = max(farthest, i + nums[i]);
 
             if(i == current_end) {
-                jumps++;
                 current_end = farthest;
+                min_jumps++;
 
                 if(current_end >= n - 1)  break;
             }
         }
 
-        return jumps;
+        return min_jumps;
     }
 };
