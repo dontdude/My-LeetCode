2cfe4677bd9a13b212ee03ff766f2d96b84e1e06
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2025-12-24 20:37:10 +0530

    Difficulty: Medium, Time: (N/A (0%)), Space: (N/A (0%))

---
+++ LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isAnagram(vector<int> shash, vector<int> phash) {
+        for(int i = 0; i < 26; i++) {
+            if(shash[i] != phash[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> phash(26, 0), shash(26, 0);
+        vector<int> res;
+
+        for(char c : p) {
+            phash[c - 'a']++;
+        }
+
+        for(int i = 0; i < p.size(); i++) {
+            shash[s[i] - 'a']++;
+        } 
+
+        if(isAnagram(phash, shash)) {
+            res.push_back(0);
+        }
+
+        for(int i = p.size(); i < s.size(); i++) {
+            int remove = i - p.size();
+            shash[s[remove] - 'a']--;
+            shash[s[i] - 'a']++;
+
+            if(isAnagram(phash, shash)) {
+                res.push_back(remove + 1);
+            }
+        }
+
+        return res;
+    }
+};
