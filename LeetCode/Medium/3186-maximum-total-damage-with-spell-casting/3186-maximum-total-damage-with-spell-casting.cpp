5525789aa6a1ab75bc3f7c17ec0ae66d003a1c65
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2025-12-25 11:33:31 +0530

    Difficulty: Medium, Time: (424 ms (22.84%)), Space: (216.3 MB (72.51%))

--- LeetCode/Medium/3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
+++ LeetCode/Medium/3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     long long maximumTotalDamage(vector<int>& power) {
+        int n =  power.size();
+        if(n == 1) return power[0];
+
         map<int, long long> count;
+
         for(int p : power) {
             count[p]++;
         }
 
+        int size = 0;
         vector<int> unique_powers;
-        for(auto c : count) {
-            unique_powers.push_back(c.first);
+        for(pair<int, int> unique_power : count) {
+            unique_powers.push_back(unique_power.first);
         }
 
-        int n = unique_powers.size();
-        vector<long long> dp(n, 0);
-        dp[0] = count[unique_powers[0]] * unique_powers[0]; 
+        vector<long long> dp(unique_powers.size(), 0);
+        dp[0] = unique_powers[0] * count[unique_powers[0]];
 
-        for(int i = 1; i < n; i++) {
-            long long take = count[unique_powers[i]] * unique_powers[i];
+        for(int i = 1; i < unique_powers.size(); i++) {
+            long long take = unique_powers[i] * count[unique_powers[i]];
 
             int j = i - 1;
-            while(j > 0 && unique_powers[j] > unique_powers[i] - 3)   j--;
-            if(unique_powers[j] <= unique_powers[i] - 3) {
-                take += dp[j];
-            }
+            while(j >= 0 && unique_powers[j] > unique_powers[i] - 3)  j--;
 
-            long long notTake = dp[i - 1];
+            if(j >= 0)  take += dp[j];
 
-            dp[i] = max(take, notTake);
+            dp[i] = max(take, dp[i - 1]);
         }
 
-        return dp[n - 1];
+        return dp.back();
     }
 };
