924dac35a33134a15baa02085ef9a98eb8d5828e
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2025-12-26 16:29:59 +0530

    Difficulty: Medium, Time: (43 ms (28.16%)), Space: (73.4 MB (14.67%))

--- LeetCode/Medium/0015-3sum/0015-3sum.js
+++ LeetCode/Medium/0015-3sum/0015-3sum.js
@@ -4,7 +4,7 @@
  */
 var threeSum = function(nums) {
     let n = nums.length;
-    var tripletSet = new Set();
+    var triplets = new Array();
 
     nums.sort((a, b) => a - b);
     for(let i = 0; i < n - 2; i++) {
@@ -13,20 +13,23 @@ var threeSum = function(nums) {
 
         while(j < k) {
             let sum = nums[i] + nums[j] + nums[k];
-            
+
             if(sum == 0) {
-                tripletSet.add(JSON.stringify([nums[i], nums[j], nums[k]]));
+                triplets.push([nums[i], nums[j], nums[k]]);
                 j++;
                 k--;
+
+                while(j < k && nums[j] == nums[j - 1]) j++;
+                while(j < k && nums[k] == nums[k + 1]) k--;
             } else if(sum > 0) {
                 k--;
+                while(j < k && nums[k] == nums[k + 1]) k--;
             } else {
                 j++;
+                while(j < k && nums[j] == nums[j - 1]) j++;
             }
         } 
     }
 
-    return Array.from(tripletSet, (triplet) => {
-        return JSON.parse(triplet);
-    });
+    return triplets;
 };
