42cffb91ea5b5a14885d569beb651356efb67980
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-01-08 10:32:14 +0530

    Difficulty: Hard, Time: (N/A (0%)), Space: (N/A (0%))

--- LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
-        if(n1 > n2)  return findMedianSortedArrays(nums2, nums1);  // cut1 should be from smaller array.. to make sure, cut2 is never negative
 
-        int low = 0, high = n1;   // high = n1, to denote cut1 can happens after all elem of nums1 too
+        if(n1 > n2)  return findMedianSortedArrays(nums2, nums1);
 
+        int low =  0, high = n1;
         while(low <= high) {
-            int cut1 = low + (high - low) / 2;  // cut1 = number of elements contributing to the Left Half
-            int cut2 = (n1 + n2 + 1) / 2 - cut1;  // with + 1, forced median to be on left
+            int x = low + (high - low) / 2;
+            int y = (n1 + n2 + 1) / 2 - x;
 
-            int l1 = (cut1 > 0) ? nums1[cut1 - 1] : INT_MIN;
-            int l2 = (cut2 > 0) ? nums2[cut2 - 1] : INT_MIN;
+            int l1 = (x > 0) ? nums1[x - 1] : INT_MIN;
+            int l2 = (y > 0) ? nums2[y - 1] : INT_MIN;
 
-            int r1 = (cut1 < n1) ? nums1[cut1] : INT_MAX;
-            int r2 = (cut2 < n2) ? nums2[cut2] : INT_MAX;
+            int r1 = (x < n1) ? nums1[x] : INT_MAX;
+            int r2 = (y < n2) ? nums2[y] : INT_MAX;
 
-            if(l1 <= r2 && l2 <= r1) {
+            if(l1 < r2 && l2 < r1) {
                 if((n1 + n2) & 1) {
                     return (double)max(l1, l2);
                 } else {
@@ -25,9 +25,9 @@ public:
                     return (double)((median1 + median2) / 2.0);
                 }
             } else if(l1 > r2) {
-                high = cut1 - 1;
+                high = x - 1;
             } else {
-                low = cut1 + 1;
+                low = x + 1;
             }
         }
 
