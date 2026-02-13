db1f2ca8bbf51a1c1dcd9c7c46eb986d23cb745e
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-02-13 10:20:13 +0530

    Difficulty: Hard, Time: (32 ms (24.91%)), Space: (85.8 MB (33.37%))

--- LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        // we require next smaller for each (on both side), so using mis (monotonic increasing stack)
+        stack<int> mis_left, mis_right;
+
         int n = heights.size();
-        vector<int> leftSmaller(n), rightSmaller(n);
-        stack<int> monotonicIncreasingStack;
+        vector<int> next_smaller_left(n), next_smaller_right(n);
 
-        for(int i = 0; i < n; i++) {
-            while(!monotonicIncreasingStack.empty() && heights[monotonicIncreasingStack.top()] >= heights[i]) {
-                monotonicIncreasingStack.pop();
+        for(int i = 0, j = n - 1; i < n, j >= 0; i++, j--) {
+            while(mis_left.empty() == false && heights[mis_left.top()] >= heights[j]) {
+                mis_left.pop();
             }
 
-            leftSmaller[i] = monotonicIncreasingStack.empty() ? -1 : monotonicIncreasingStack.top();
-            monotonicIncreasingStack.push(i);
-        }
-
-        while(!monotonicIncreasingStack.empty())  monotonicIncreasingStack.pop();
-
-        for(int i = n - 1; i >= 0; i--) {
-            while(!monotonicIncreasingStack.empty() && heights[monotonicIncreasingStack.top()] >= heights[i]) {
-                monotonicIncreasingStack.pop();
+            while(mis_right.empty() == false && heights[mis_right.top()] >= heights[i]) {
+                mis_right.pop();
             }
 
-            rightSmaller[i] = monotonicIncreasingStack.empty() ? n : monotonicIncreasingStack.top();
-            monotonicIncreasingStack.push(i);
+            next_smaller_left[j] = mis_left.empty() ? n : mis_left.top();
+            next_smaller_right[i] = mis_right.empty() ? -1 : mis_right.top(); 
+
+            mis_left.push(j);
+            mis_right.push(i);
         }
 
-        int res = 0;
+        int maxArea = 0;
         for(int i = 0; i < n; i++) {
             int breadth = heights[i];
-            int length = rightSmaller[i] - leftSmaller[i] - 1;
+            int length = next_smaller_left[i] - next_smaller_right[i] - 1;
+            maxArea = max(maxArea, length * breadth);
+        }
 
-            res = max(res, breadth * length); 
-        } 
-
-        return res;
+        return maxArea;
     }
 };
