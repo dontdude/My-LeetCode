1ae5bf40f47a4c9137e187169a2c761f117868ed
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-03-08 10:07:30 +0530

    Difficulty: Hard, Time: (0 ms (100%)), Space: (25.9 MB (92.99%))

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size();
-
-        int l = 0, r = n - 1;
-        int lmax = height[l], rmax = height[r];
-
+        int l = 0, r = height.size() - 1;
+        int lmax = height[0], rmax = height.back();
         int water = 0;
+
         while(l <= r) {
+            lmax = max(lmax, height[l]);
+            rmax = max(rmax, height[r]);
+
             int moving;
-            if(height[l] < height[r]) {
-                moving = height[l];
-                l++;
+            if(lmax < rmax) {
+               moving = l;
+               l++; 
             } else {
-                moving = height[r];
+                moving = r;
                 r--;
             }
 
-            water += max(0, min(lmax, rmax) - moving);
-            lmax = max(lmax, height[l]);
-            rmax = max(rmax, height[r]);
+            water += min(lmax, rmax) - height[moving];
         }
 
         return water;
