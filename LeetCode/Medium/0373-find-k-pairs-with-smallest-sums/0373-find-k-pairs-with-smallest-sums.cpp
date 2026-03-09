654f7af270809a80ca27ed1e8c65e3906ddcdabc
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-03-09 23:09:23 +0530

    Difficulty: Medium, Time: (55 ms (55.96%)), Space: (147.9 MB (22.43%))

--- LeetCode/Medium/0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ LeetCode/Medium/0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,42 +1,25 @@
 class Solution {
-    struct SumPairs {
-        int sum;
-        int u;
-        int v;
-
-        SumPairs(int s, int i, int j) {
-            sum = s;
-            u = i;
-            v = j;
-        }
-    };
-
-    struct comparator {
-        bool operator()(SumPairs a, SumPairs b) {
-            return a.sum > b.sum;
-        }
-    };
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int m = nums1.size(), n = nums2.size();
+        priority_queue<pair<int, pair<int, int>>> minheap;   // {-sum, {i, j}}
 
-        priority_queue<SumPairs, vector<SumPairs>, comparator> pq;
+        for(int i = 0; i < nums1.size(); i++) {
+            int sum = nums1[i] + nums2[0];
+            minheap.push({-sum, {i, 0}});
+        }
 
         vector<vector<int>> res;
 
-        for(int i = 0; i < m && i < k; i++) {
-            pq.push({nums1[i] + nums2[0], i, 0});
-        }
+        while(!minheap.empty() && res.size() < k) {
+            int i = minheap.top().second.first;
+            int j = minheap.top().second.second;
+            minheap.pop();
 
-        while(res.size() < k && pq.empty() == false) {
-            int u = pq.top().u;
-            int v = pq.top().v;
-            pq.pop();
-            
-            res.push_back({nums1[u], nums2[v]});
+            res.push_back({nums1[i], nums2[j]});
 
-            if(v + 1 < n) {
-                pq.push({nums1[u] + nums2[v + 1], u, v + 1});
+            if(j + 1 < nums2.size()) {
+                int sum = nums1[i] + nums2[j + 1];
+                minheap.push({-sum, {i, j + 1}});
             }
         }
 
