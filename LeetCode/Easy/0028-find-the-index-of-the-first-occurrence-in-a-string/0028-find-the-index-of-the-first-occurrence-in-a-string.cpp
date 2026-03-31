16d29ddd8dfb2588e1a4dc27e18eb94dd450c432
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-03-31 21:04:04 +0530

    Difficulty: Easy, Time: (N/A (0%)), Space: (N/A (0%))

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,44 +1,55 @@
 class Solution {
-    void constructLPS(int& n, string& pat, vector<int>& lps) {
-        lps[0] = 0;
-        int i = 1, prevLPS = 0;
+    long long power(int base, int pow, int mod) {
+        if(pow == 0)  return 1;
+        if(pow == 1)  return base;
 
-        while(i < n) {
-            if(pat[i] == pat[prevLPS]) {
-                lps[i++] = ++prevLPS;
-            } else {
-                if(prevLPS == 0) {
-                    lps[i++] = 0;
-                } else {
-                    prevLPS = lps[prevLPS - 1];
-                }
+        long long halfPower = power(base, (pow >> 1), mod);
+
+        long long curr = (halfPower * halfPower) % mod;
+
+        return (pow & 1) ? ((curr * base) % mod) : curr;
+    }
+    bool patFound(int start, string& txt, string& pat) {
+        for(int j = 0; j < pat.size(); j++) {
+            if(pat[j] != txt[j + start]) return false;
+        }
+        return true;
+    }
+    int RabinKarp(int m, int n, string& txt, string& pat) {
+        if(m < n)  return -1;
+
+        int d = 26; // lowercase only
+        long long mod = 1e9 + 7;
+
+        long long h = power(d, n - 1, mod);
+
+        long long phash = 0;
+        long long thash = 0;
+
+        for(int j = 0; j < n; j++) {
+            phash = ((phash * d) + (pat[j] - 'a')) % mod;
+            thash = ((thash * d) + (txt[j] - 'a')) % mod;
+        }
+
+        if(phash == thash && patFound(0, txt, pat)) {
+            return 0;
+        }
+
+        for(int i = n; i < m; i++) {
+            thash = (thash - (h * (txt[i - n] - 'a')) % mod + mod) % mod;
+            thash = ((thash * d) + (txt[i] - 'a')) % mod;
+
+            if(phash == thash && patFound(i - n, txt, pat)) {
+                return i - n;
             }
         }
+
+        return -1;
     }
 public:
     int strStr(string haystack, string needle) {
         int m = haystack.size(), n = needle.size();
 
-        vector<int> lps(n);
-        constructLPS(n, needle, lps);
-
-        int i = 0, j = 0;
-
-        while(i < m) {
-            if(haystack[i] == needle[j]) {
-                i++;
-                j++;
-
-                if(j == n) return i - n;
-            } else {
-                if(j == 0) {
-                    i++;
-                } else {
-                    j = lps[j - 1];
-                }
-            }
-        }
-
-        return -1;
+        return RabinKarp(m, n, haystack, needle);
     }
 };
