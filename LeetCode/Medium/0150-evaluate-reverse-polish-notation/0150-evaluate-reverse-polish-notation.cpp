373d2636fc944541df90a5a455971a15b3bdf663
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-13 20:58:52 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (16 MB (90.69%))

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,34 +1,66 @@
 class Solution {
-public:
-    int applyOperation(int n1, int n2, string op) {
-        switch (op[0]) {
-            case '+': return n2 + n1;
-            case '-': return n2 - n1;
-            case '*': return n2 * n1;
-            case '/': return n2 / n1;
+    char operators[4] = {'+', '-', '*', '/'};
+    bool isOperator(string& token) {
+        if(token.size() != 1)  return false;
+        
+        for(int k = 0; k < 4; k++) {
+            if(token[0] == operators[k]) return true;
         }
 
+        return false;
+    }
+
+    int tokenToNumber(string token) {
+        if(token.size() < 1)  return 0;
+
+        int sign = 1, num = 0;
+        
+        int i = 0;
+        if(token[i] == '-') {
+            sign = -1;
+            i++;
+        }
+
+        while(i < token.size()) {
+            int d = token[i] - '0';
+            num *= 10;
+            num += d;
+            i++;
+        }
+
+        return sign * num;
+    }
+
+    int calculate(int num1, int num2, string sign) {
+        if(sign[0] == '+')  return num1 + num2;
+        if(sign[0] == '-')  return num1 - num2;
+        if(sign[0] == '*')  return num1 * num2;
+        if(sign[0] == '/')  return num1 / num2;
         return 0;
     }
+public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> st;
-        vector<string> operators = {"+", "-", "*", "/"};
+        stack<int> operands;
 
-        for(string token: tokens) {
-            if(token == "+" || token == "-" || token == "*" || token == "/") {
-                int number1 = st.top();  
-                st.pop();
+        for(string token : tokens) {
+            int num;
+            if(isOperator(token)) {
+                if(operands.size() < 2)  return 0;
 
-                int number2 = st.top();
-                st.pop();
+                int num2 = operands.top();
+                operands.pop();
 
-                int number3 = applyOperation(number1, number2, token);
-                st.push(number3); 
+                int num1 = operands.top();
+                operands.pop();
+
+                num = calculate(num1, num2, token);
             } else {
-                st.push(stoi(token));
+                num = tokenToNumber(token);
             }
+
+            operands.push(num);
         }
 
-        return st.top();
+        return operands.top();
     }
 };
