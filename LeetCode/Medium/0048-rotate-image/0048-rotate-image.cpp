ef7906d085a053af83d4649be3519e1c891ca2e4
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-16 19:44:12 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (10.2 MB (35.99%))

--- LeetCode/Medium/0048-rotate-image/0048-rotate-image.cpp
+++ LeetCode/Medium/0048-rotate-image/0048-rotate-image.cpp
@@ -1,32 +1,32 @@
 class Solution {
-    // The final matrix is basically.. row and column swapped, and each row element reversed
-    
-    void transpose(vector<vector<int>>& matrix) {
-        for(int i = 1; i < matrix.size(); i++) {
-            for(int j = 0; j < i; j++) {
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-    }
-
-    void reverseRows(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        for(int i = 0; i < n; i++) {
-            int l = 0, r = n - 1;
-
-            while(l < r) {
-                swap(matrix[i][l], matrix[i][r]);
-                l++;
-                r--;
-            }
-        }
-    }
 public:
+    // Credit for this intutive solution : Neetcode
     void rotate(vector<vector<int>>& matrix) {
-        // Swap row and col (transpose)
-        transpose(matrix);
+        int n = matrix.size();
+        int l = 0, r = n - 1;
 
-        // Rever elements of each row
-        reverseRows(matrix);
+        while(l < r) {
+            int t = l, b = r;  // since n * n
+
+            for(int i = 0; i < (r - l); i++) {
+                // store top left
+                int topLeft = matrix[t][l + i];
+
+                // move bottom left -> top left
+                matrix[t][l + i] = matrix[b - i][l];
+
+                // move bottom right -> bottom left 
+                matrix[b - i][l] = matrix[b][r - i];
+
+                // move top right -> bottom right
+                matrix[b][r - i] = matrix[t + i][r];
+
+                // move top left to -> top right
+                matrix[t + i][r] = topLeft;
+            }
+
+            l++;
+            r--;
+        }
     }
 };
