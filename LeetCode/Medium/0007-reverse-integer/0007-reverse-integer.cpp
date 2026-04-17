31d28319404c2ee36f382ba5f44ce7b504033a69
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-18 00:15:57 +0530

    Difficulty: Medium, Time: (3 ms (28.96%)), Space: (8.5 MB (82.6%))

---
+++ LeetCode/Medium/0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    bool isSafe(int num, int digit, int limit) {
+        int truncatedDigit = limit % 10;
+        int restLimit = limit / 10;
+        if(limit == INT_MIN)  return num > restLimit || (num == restLimit && digit >= truncatedDigit);
+        return num < restLimit || (num == restLimit && digit <= truncatedDigit);
+    }
+public:
+    int reverse(int x) {
+        int limit = (x > 0) ? INT_MAX : INT_MIN;
+        int revx = 0, sign = 1;
+
+        while(x) {
+            int d = x % 10;
+            x /= 10;
+
+            if(d == 0 && revx == 0) continue;
+
+            if(isSafe(revx, d, limit) == false) {
+                return 0;
+                
+            } 
+            
+            revx *= 10;
+            revx += d;
+        }
+
+        return revx;
+    }
+};
