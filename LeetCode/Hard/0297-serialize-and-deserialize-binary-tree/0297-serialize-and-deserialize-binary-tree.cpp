ad8697326bf1a35e708ea61686ddb0c296ce9780
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-17 21:40:32 +0530

    Difficulty: Hard, Time: (96 ms (5.06%)), Space: (523.7 MB (5.01%))

--- LeetCode/Hard/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
+++ LeetCode/Hard/0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.cpp
@@ -8,26 +8,37 @@
  * };
  */
 class Codec {
+    string getNodeToken(string& s) {
+        string nodeToken = "";
+        int i = 0;
+        while(i < s.size() && s[i] != ',') {
+            nodeToken.push_back(s[i++]);
+        }
+
+        s = s.substr(i + 1);
+
+        return nodeToken;
+    }
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(root == NULL)  return ""; 
-
         string serialized = "";
-        queue<TreeNode*> q;
 
+        queue<TreeNode*> q;
         q.push(root);
 
-        while(!q.empty()) {  // level order serializer
-            TreeNode* node = q.front();    q.pop();
+        while(!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
 
             if(node == NULL) {
-                serialized += "#,";
+                serialized.append("#,");
                 continue;
             }
 
-            serialized += to_string(node->val) + ",";
+            string val = to_string(node->val);
+            serialized.append(val + ",");
 
             q.push(node->left);
             q.push(node->right);
@@ -38,33 +49,31 @@ public:
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-        if(data.size() == 0)  return NULL;
+        string rootToken = getNodeToken(data);
+        if(rootToken == "#") return NULL;
+
+        TreeNode* root = new TreeNode(stoi(rootToken));
 
         queue<TreeNode*> q;
-        stringstream s(data);
-        string curs;
-        getline(s, curs, ',');
-
-        TreeNode* root = new TreeNode(stoi(curs));
         q.push(root);
 
         while(!q.empty()) {
-            TreeNode* node = q.front();   q.pop();
+            TreeNode* node = q.front();
+            q.pop();
 
-            getline(s, curs, ',');
-            if(curs == "#") {
-                node->left = NULL;
-            } else if(curs.size() > 0) {
-                node->left = new TreeNode(stoi(curs));
-                q.push(node->left);
+            string leftToken = getNodeToken(data);
+            string rightToken = getNodeToken(data);
+
+            if(leftToken != "#") {
+                TreeNode* left = new TreeNode(stoi(leftToken));
+                node->left = left;
+                q.push(left);
             }
 
-            getline(s, curs, ',');
-            if(curs == "#") {
-                node->right = NULL;
-            } else if(curs.size() > 0) {
-                node->right = new TreeNode(stoi(curs));
-                q.push(node->right);
+            if(rightToken != "#") {
+                TreeNode* right = new TreeNode(stoi(rightToken));
+                node->right = right;
+                q.push(right);
             }
         }
 
