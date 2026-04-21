3969d9d40ac1f47ebc6d2ba3f4688917a7862cf0
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-21 12:07:52 +0530

    Difficulty: Medium, Time: (45 ms (5.12%)), Space: (20 MB (33.03%))

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.cpp
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.cpp
@@ -1,72 +1,42 @@
 class Solution {
-public:
-    // BFS Solution (Kahn's algorthm)
-    // bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    //     vector<vector<int>> graph(numCourses);
-    //     vector<int> in_degree(numCourses, 0);
-
-    //     for (auto prereq : prerequisites) {
-    //         graph[prereq[1]].push_back(prereq[0]);
-    //         in_degree[prereq[0]]++;
-    //     }
-
-    //     int doableCourses = 0;
-    //     queue<int> q;
-
-    //     for (int i = 0; i < numCourses; i++) {
-    //         if (in_degree[i] == 0) {
-    //             q.push(i);
-    //             in_degree[i]--;
-    //             doableCourses++;
-    //         }
-    //     }
-
-    //     while (!q.empty()) {
-    //         int node = q.front();
-    //         q.pop();
-
-    //         for(int nebr: graph[node]) {
-    //             in_degree[nebr]--;
-    //             if(in_degree[nebr] == 0) {
-    //                 q.push(nebr);
-    //                 doableCourses++;
-    //             }
-    //         }
-    //     }
-
-    //     return numCourses == doableCourses;
-    // }
-
-    // DFS
-    bool hasCycle(int node, vector<vector<int>>& graph, vector<int>& vis) {
-        vis[node] = 1;
-        for(int nebr: graph[node]) {
-            if(vis[nebr] == 0) {
-                if(hasCycle(nebr, graph, vis))  return true;
-            } else if(vis[nebr] == 1) {  // reaches any node, in current connected componnets
-                return true;
-            } // else vis[nebr] == 2, means, it is already visited in another connection and doen't have any cycle
-        }
-        vis[node] = 2;
-
-        return false;
-    }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> graph(numCourses);
-        vector<int> vis(numCourses, 0);
+    bool topoSort(vector<vector<int>>& graph, vector<int>& indegree, int n) {
+        queue<int> q;
+        int processed = 0;
         
-        for(auto prereq: prerequisites) {
-            graph[prereq[1]].push_back(prereq[0]);
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+                indegree[i]--;
+                processed++;
+            }
         }
 
-        for(int i = 0; i < numCourses; i++) {
-            if(vis[i] == 0) {
-                if(hasCycle(i, graph, vis)) {
-                    return false;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            for(int nebr: graph[node]) {
+                indegree[nebr]--;
+                if(indegree[nebr] == 0) {  cout<<node<<" "<<nebr<<endl;
+                    q.push(nebr);
+                    indegree[nebr]--;
+                    processed++;
                 }
             }
         }
 
-        return true;
+        return processed == n;
+    }
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses);
+
+        for(vector<int> preq : prerequisites) {
+            graph[preq[1]].push_back(preq[0]);
+            indegree[preq[0]]++;
+        }
+
+        return topoSort(graph, indegree, numCourses);
     }
 };
