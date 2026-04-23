3532ec29a8c3c59ac8d968622e1a361d4ecdc512
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-23 13:08:55 +0530

    Difficulty: Medium, Time: (12 ms (22.32%)), Space: (10.8 MB (70.01%))

---
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int left = 0, right = 0, maxOcc = 0, result = 0;
+        vector<int> count(26, 0);
+
+        while(right < s.size()) {
+            count[s[right] - 'A']++;
+            maxOcc = max(maxOcc, count[s[right] - 'A']);
+            right++;
+
+            while((right - left) - maxOcc > k) {
+                count[s[left] - 'A']--;
+                left++;
+
+                maxOcc = *max_element(count.begin(), count.end());   // updating max occ each time
+            } 
+
+            result = max(result, right - left);
+        }
+
+        return result;
+    }
+};
