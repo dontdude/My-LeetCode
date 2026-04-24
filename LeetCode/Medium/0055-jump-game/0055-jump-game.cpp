534b08dd260e07c3332dc7108bd7de1442efd833
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-24 13:05:18 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (52.4 MB (56.2%))

--- LeetCode/Medium/0055-jump-game/0055-jump-game.cpp
+++ LeetCode/Medium/0055-jump-game/0055-jump-game.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int maxReach = 0;
+        int n = nums.size();
+        if(n <= 1)  return true;
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(i <= maxReach) {
-                maxReach = max(maxReach, i + nums[i]);
-            } else {
-                return false;
+        int nextJump = 0, currEnd = 0, jumps = 0;
+
+        for(int i = 0; i < n && currEnd < n - 1; i++) { 
+            nextJump = max(nextJump, i + nums[i]);
+            
+            if(i == currEnd) {
+                if(nextJump <= currEnd) return false;  
+                jumps++;
+                currEnd = nextJump;
             }
         }
 
-        return true;
+        return currEnd >= n-1;
     }
 };
