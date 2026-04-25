eda292d3aa08b0e316f843b905c5692243204ba0
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-25 09:39:31 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (21.7 MB (74.78%))

--- LeetCode/Medium/0057-insert-interval/0057-insert-interval.cpp
+++ LeetCode/Medium/0057-insert-interval/0057-insert-interval.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> res;
+        vector<vector<int>> merged;
 
         int i = 0, n = intervals.size();
 
-        while(i < n && intervals[i][1] < newInterval[0]) {  // Phase 1
-            res.push_back(intervals[i++]);
+        while(i < n && intervals[i][1] < newInterval[0]) {
+            merged.push_back(intervals[i]);
+            i++;
         }
 
-        if(i < n) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);   // current overlapping may have smaller start
-            // not updating end, because it will fail if newInterval was our 1st going to be interval, levae it with while loop check
+        while(i < n && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
 
-        while(i < n && intervals[i][0] <= newInterval[1]) {  // phase 2
-            newInterval[1] = max(newInterval[1], intervals[i++][1]);
+        merged.push_back(newInterval);
+
+        while(i < n) {
+            merged.push_back(intervals[i]);
+            i++;
         }
-
-        res.push_back(newInterval);
-
-        while(i < n) {  // phase 3
-            res.push_back(intervals[i++]);  
-        }
-
-        return res;
+        
+        return merged;
     }
 };
