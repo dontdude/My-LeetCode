b78cc0498a3475bfab2ecda072059c428ef764a9
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-27 22:05:39 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (10 MB (88.05%))

--- LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n + 1, 0);
+        int prev1 = 0, prev2 = nums[0];
 
-        dp[1] = nums[0];
-
-        for(int i = 2; i <= n; i++) {
-            dp[i] = max(dp[i - 1], nums[i - 1] + dp[i - 2]);
+        for(int i = 1; i < n; i++) {
+            int curr = max(prev2, nums[i] + prev1);
+            prev1 = prev2;
+            prev2 = curr;
         }
 
-        return dp[n];
+        return prev2;
     }
 };
