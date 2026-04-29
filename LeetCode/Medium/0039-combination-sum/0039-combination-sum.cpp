570fd392e20bc8834b66d21718d71535ec5d8aef
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-29 23:13:47 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (14.1 MB (72.39%))

--- LeetCode/Medium/0039-combination-sum/0039-combination-sum.cpp
+++ LeetCode/Medium/0039-combination-sum/0039-combination-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
-    void solve(int i, vector<int>& curr, vector<vector<int>>& res, int t, vector<int>& c) {
-        if(t == 0) {
-            res.push_back(curr);
+    void helper(int i, int target, vector<int>& combination, vector<vector<int>>& result, vector<int>& candidates) {
+        if(target == 0) {
+            result.push_back(combination);
             return;
         }
-        
-        if(t < 0 || i < 0) return;
 
-        // take it
-        curr.push_back(c[i]);
-        solve(i, curr, res, t - c[i], c);
+        if(i == candidates.size()) return;
 
-        // leave it
-        curr.pop_back();
-        solve(i - 1, curr, res, t, c);
+        // take
+        combination.push_back(candidates[i]);
+        if(target >= candidates[i]) helper(i, target - candidates[i], combination, result, candidates);
+
+        // not take
+        combination.pop_back();
+        helper(i + 1, target, combination, result, candidates);
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        int n = candidates.size();
-        vector<vector<int>> res;
-        vector<int> curr;
-        solve(n - 1, curr, res, target, candidates);
-        return res;
+        vector<vector<int>> result;
+        vector<int> combination;
+        helper(0, target, combination, result, candidates);
+        return result;
     }
 };
