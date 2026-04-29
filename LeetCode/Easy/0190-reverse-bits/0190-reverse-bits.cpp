123839eef345ac3b0cd3e99afb6d79d1e9963e9b
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-29 23:31:02 +0530

    Difficulty: Easy, Time: (N/A (0%)), Space: (N/A (0%))

--- LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.cpp
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,11 +1,23 @@
 class Solution {
+    // followup optimization:
+    vector<int> powerOf2;
+
+    void processPower(int s) {
+        powerOf2.resize(s);
+        for(int i = 0; i < s; i++) {
+            powerOf2[i] = (1 << i);
+        }
+    }
 public:
+    Solution() {
+        processPower(32);
+    }
     int reverseBits(int n) {
         int rev = 0, s = 31;
 
         for(int i = 0; i <= s; i++) {
-            if(n & (1 << i)) {
-                rev = (rev | (1 << (s - i)));
+            if(n & powerOf2[i]) {
+                rev = (rev | powerOf2[s - i]);
             }
         }
 
