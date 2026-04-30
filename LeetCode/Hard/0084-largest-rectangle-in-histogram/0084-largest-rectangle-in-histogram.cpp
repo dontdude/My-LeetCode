8afeb3557485d65ecf82283d731574bbec1ca498
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-04-30 23:30:40 +0530

    Difficulty: Hard, Time: (27 ms (37.91%)), Space: (85.9 MB (22.87%))

--- LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        // we require next smaller for each (on both side), so using mis (monotonic increasing stack)
-        stack<int> mis_left, mis_right;
-
         int n = heights.size();
-        vector<int> next_smaller_left(n), next_smaller_right(n);
+        stack<int> left_mis, right_mis;
+        vector<int> left_smaller(n), right_smaller(n);
 
-        for(int i = 0, j = n - 1; i < n, j >= 0; i++, j--) {
-            while(mis_left.empty() == false && heights[mis_left.top()] >= heights[j]) {
-                mis_left.pop();
+        for(int i = 0; i < n; i++) {
+            while(!left_mis.empty() && heights[left_mis.top()] >= heights[i]) {
+                left_mis.pop();
             }
 
-            while(mis_right.empty() == false && heights[mis_right.top()] >= heights[i]) {
-                mis_right.pop();
+            int j = n - 1 - i;
+            while(!right_mis.empty() && heights[right_mis.top()] >= heights[j]) {
+                right_mis.pop();
             }
 
-            next_smaller_left[j] = mis_left.empty() ? n : mis_left.top();
-            next_smaller_right[i] = mis_right.empty() ? -1 : mis_right.top(); 
+            if(left_mis.empty()) left_smaller[i] = -1;
+            else left_smaller[i] = left_mis.top();
 
-            mis_left.push(j);
-            mis_right.push(i);
+            if(right_mis.empty()) right_smaller[j] = n;
+            else right_smaller[j] = right_mis.top();
+
+            left_mis.push(i);
+            right_mis.push(j);
         }
 
         int maxArea = 0;
         for(int i = 0; i < n; i++) {
-            int breadth = heights[i];
-            int length = next_smaller_left[i] - next_smaller_right[i] - 1;
-            maxArea = max(maxArea, length * breadth);
+            int len = heights[i];
+            int wid = right_smaller[i] - left_smaller[i] - 1;
+            maxArea = max(maxArea, len * wid);
         }
 
         return maxArea;
