b6ac7a6d0e0d9b874e3368de289f2a17b3cdff1a
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-04 10:18:05 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (15.3 MB (34.05%))

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,10 +8,10 @@ public:
 
             if(nums[mid] == target) {
                 return mid;
-            } else if(nums[left] <= nums[mid]) {
+            } else if(nums[left] <= nums[mid]) {  // left is sorted
                 if(nums[left] <= target && target < nums[mid])  right = mid - 1;
-                else left = mid + 1; 
-            } else {
+                else left = mid + 1;
+            } else {  // right is sorted
                 if(nums[mid] < target && target <= nums[right])  left = mid + 1;
                 else right = mid - 1;
             }
