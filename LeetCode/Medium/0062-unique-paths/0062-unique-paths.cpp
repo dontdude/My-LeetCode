79bd784223c65803c4b30679d0badd2b5c28dd25
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-04 09:35:59 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (8.3 MB (79.57%))

--- LeetCode/Medium/0062-unique-paths/0062-unique-paths.cpp
+++ LeetCode/Medium/0062-unique-paths/0062-unique-paths.cpp
@@ -1,39 +1,18 @@
 class Solution {
 public:
-    // int dfs(int i, int j, int m, int n) {  // i = 0 -> m, j = 0 -> n
-    //     if(i == m - 1 && j == n - 1) {
-    //         return 1;
-    //     }
-
-    //     int paths = 0;
-        
-    //     if(i + 1 < m)  paths += dfs(i + 1, j, m, n);
-    //     if(j + 1 < n)  paths += dfs(i, j + 1, m, n);
-
-    //     return paths;
-    // }
-
-    // Tabulation
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> prev(n, 0), curr(n, 0);
+        prev[0] = 1;
 
-        for(int i = m - 1; i >= 0; i--) {
-            for(int j = n - 1; j >= 0; j--) {
-
-                if(i == m - 1 && j == n - 1)  {
-                    dp[i][j] = 1;
-                    continue;
-                }
-
-                int paths = 0;
-        
-                if(i + 1 < m)  paths += dp[i + 1][j];
-                if(j + 1 < n)  paths += dp[i][j + 1];
-
-                dp[i][j] = paths;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                curr[j] = prev[j];
+                if(j) curr[j] += curr[j - 1];
             }
+
+            prev = curr;
         }
 
-        return dp[0][0];
+        return prev[n - 1];
     }
 };
