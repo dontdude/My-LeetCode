aeac4ca0105a05e130daa42bc14ac73c81b3e5eb
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-05 22:21:59 +0530

    Difficulty: Medium, Time: (N/A (0%)), Space: (N/A (0%))

--- LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
+++ LeetCode/Medium/0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,24 @@
 class Solution {
+    string getFreqKey(string& str) {
+        string freq(26, '0');
+
+        for(char c : str) freq[c - 'a']++;
+
+        return freq;
+    }
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, int> hashmap;
         vector<vector<string>> result;
 
         for(string str : strs) {
-            string sortedStr = str;
-            sort(sortedStr.begin(), sortedStr.end());
+            string freqKey = getFreqKey(str);
 
-            if(hashmap.find(sortedStr) == hashmap.end()) {
-                hashmap[sortedStr] = result.size();
+            if(hashmap.find(freqKey) == hashmap.end()) {
+                hashmap[freqKey] = result.size();
                 result.push_back({str});
             } else {
-                int indexItBelongTo = hashmap[sortedStr];
+                int indexItBelongTo = hashmap[freqKey];
                 result[indexItBelongTo].push_back(str);
             }
         }
