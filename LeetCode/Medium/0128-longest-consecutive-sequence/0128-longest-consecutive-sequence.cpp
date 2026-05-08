a9e631ee38f0c3105703e4b75b94ca9949d52f50
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-08 10:43:28 +0530

    Difficulty: Medium, Time: (213 ms (5.03%)), Space: (119.8 MB (5.16%))

--- LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ LeetCode/Medium/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,20 +1,60 @@
 class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> hash(nums.begin(), nums.end());
-        int result = 0;
+    class DSU {
+        int maxSizeTree;
+        unordered_map<int, int> parent;
+        unordered_map<int, int> size;
 
-        for(int num : hash) {
-            if(hash.find(num - 1) != hash.end())  continue;
-            int seqStartingFromCurr = 1;
-            while(hash.find(num + 1) != hash.end()) {
-                seqStartingFromCurr++;
-                num++;
-            }
-
-            result = max(result, seqStartingFromCurr);
+    public:
+        DSU() {
+            maxSizeTree = 0;
         }
 
-        return result;
+        int getMaxSize() {
+            return maxSizeTree;
+        }
+
+        void add(int num) {
+            if(parent.find(num) != parent.end())  return;
+            parent[num] = num;
+            size[num] = 1;
+            maxSizeTree = max(maxSizeTree, size[num]);
+        }
+
+        int find(int num) {
+            if(parent[num] == num)  return num;
+
+            return parent[num] = find(parent[num]);
+        }
+
+        void unite(int i, int j) {
+            if(parent.find(j) == parent.end())  return;
+            int upi = find(i);
+            int upj = find(j);
+
+            if(upi == upj) return;
+
+            if(size[upi] < size[upj]) {
+                parent[upj] = upi;
+                size[upi] += size[upj];
+                maxSizeTree = max(maxSizeTree, size[upi]);
+            } else {
+                parent[upi] = upj;
+                size[upj] += size[upi];
+                maxSizeTree = max(maxSizeTree, size[upj]);
+            }
+        }
+    };
+public:
+    int longestConsecutive(vector<int>& nums) {
+        DSU dsu;
+
+        for(int num : nums) {
+            dsu.add(num);
+
+            dsu.unite(num, num + 1);
+            dsu.unite(num, num - 1);
+        }
+
+        return dsu.getMaxSize();
     }
 };
