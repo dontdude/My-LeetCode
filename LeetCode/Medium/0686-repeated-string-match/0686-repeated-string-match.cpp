98c7457595a2937ac39e498858eb4ad6f5a4915c
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-22 09:10:58 +0530

    Difficulty: Medium, Time: (N/A (0%)), Space: (N/A (0%))

--- LeetCode/Medium/0686-repeated-string-match/0686-repeated-string-match.cpp
+++ LeetCode/Medium/0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,59 +1,51 @@
 class Solution {
-    long long power(int base, int exponent, int modulo) {
-        if(exponent == 0)  return 1;
-        if(exponent == 1)  return (long long)base;
+    long long modularExponentiation(int base, int exponent, int mod) {
+        if(exponent == 0) return 1LL;
+        if(exponent == 1) return (long long)base;
 
-        long long halfPower = power(base, exponent / 2, modulo);
-        long long result = (halfPower * halfPower) % modulo;
+        long long halfExpo = modularExponentiation(base, exponent / 2, mod);
+
+        long long res = (halfExpo * halfExpo) % mod;
 
         if(exponent & 1) {
-            result = (base * result) % modulo;
+            res = (res * base) % mod;
         }
 
-        return result;
+        return res;
     }
-    bool isPatFound(int start, string& text, string& pat) {
-        int n = pat.size();
-
-        for(int i = 0; i < n; i++) {
-            if(text[i + start] != pat[i])  return false;
+    bool patternMatch(int start, string& text, string& pat) {
+        for(int i = 0; i < pat.size(); i++) {
+            if(text[i + start] != pat[i]) return false;
         }
-
         return true;
     }
-    bool RabinKarpSearch(string& text, string& pat) {
+    bool RabinKarpStringMatch(string& text, string& pat) {
         int m = text.size(), n = pat.size();
+        if(m < n) return false;
 
-        if (m < n) return false;
+        int mod = 1e9 + 7;
+        int d = 26; // base 26 based digit addition
         
-        int d = 26;   // strings consist of lowercase letters only
-        long long q = 1e9 + 7;   // large prime number for modulo
-
-        long long h = power(d, n - 1, q);
+        long long h = modularExponentiation(d, n - 1, mod);
         long long phash = 0;
         long long thash = 0;
 
         int i = 0;
         while(i < n) {
-            phash = ((d * phash) + pat[i]) % q;
-            thash = ((d * thash) + text[i]) % q;
+            phash = ((phash * d) + pat[i]) % mod;
+            thash = ((thash * d) + text[i]) % mod;
             i++;
         }
 
-        if(phash == thash && isPatFound(i - n, text, pat)) {
-            return true;
-        }
+        if(thash == phash && patternMatch(0, text, pat))  return true;
 
         while(i < m) {
-            int currWindowStart = i - n;
+            int windowStart = i - n;
 
-            thash = (thash - (h * (text[currWindowStart]) % q) + q) % q;   // removing shifted window char
+            thash = (thash - (h * text[windowStart]) + mod) % mod;
+            thash = ((thash * d) + text[i]) % mod;
 
-            thash = ((d * thash) +  text[i]) % q;  // adding new window char
-
-            if(phash == thash && isPatFound(currWindowStart + 1, text, pat)) {
-                return true;
-            }
+            if(thash == phash && patternMatch(windowStart + 1, text, pat))  return true;
 
             i++;
         }
@@ -62,28 +54,25 @@ class Solution {
     }
 public:
     int repeatedStringMatch(string a, string b) {
-        int len_a = a.size();
-        int len_b = b.size();
-
-        int n = len_b / len_a;
+        int n = b.size() / a.size();
 
         string na = "";
         for(int i = 0; i < n; i++) {
-            na += a;
+            na.append(a);
         }
 
-        if(RabinKarpSearch(na, b)) {
-            return n;
-        }
-
-        na += a;
-        if(RabinKarpSearch(na, b)) {
+        if(RabinKarpStringMatch(na, b)) {
             return n + 1;
         }
 
-        na += a;
-        if(RabinKarpSearch(na, b)) {
+        na.append(a);
+        if(RabinKarpStringMatch(na, b)) {
             return n + 2;
+        }
+
+        na.append(a);
+        if(RabinKarpStringMatch(na, b)) {
+            return n + 3;
         }
 
         return -1;
