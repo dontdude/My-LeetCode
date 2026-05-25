57c0abbb6de9d7cb461b65ef394a1c07684ae359
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-25 10:00:21 +0530

    Difficulty: Medium, Time: (1 ms (92.85%)), Space: (22.7 MB (63.47%))

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,46 +1,57 @@
 class Solution {
-    int m, n;
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
 
-    bool isSafe(int x, int y) {
-        return (x >= 0 && x < m) && (y >= 0 && y < n);
+    bool isValid(int x, int y, int ocean, vector<vector<int>>& grid) {
+        return (x >= 0 && x < grid.size()) && (y >= 0 && y < grid[0].size()) && grid[x][y] != ocean && grid[x][y] != 0;
     }
 
-    void dfs(int i, int j, vector<vector<bool>>& vis, vector<vector<int>>& h) {
-        if(vis[i][j])  return;
-
-        vis[i][j] = true;
+    void dfs(int i, int j, int ocean, vector<vector<int>>& grid, vector<vector<int>>& heights) {
+        if(grid[i][j] == INT_MAX) grid[i][j] = ocean;
+        else if(grid[i][j] != ocean) grid[i][j] += ocean;
 
         for(int k = 0; k < 4; k++) {
             int x = i + dx[k];
             int y = j + dy[k];
 
-            if(isSafe(x, y) && !vis[x][y] && h[x][y] >= h[i][j]) {
-                dfs(x, y, vis, h);
+            if(isValid(x, y, ocean, grid) && heights[i][j] <= heights[x][y]) {
+                dfs(x, y, ocean, grid, heights);
             }
         }
     }
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        m = heights.size(), n = heights[0].size();
+        int m = heights.size(), n = heights[0].size();
+        vector<vector<int>> grid(m, vector<int>(n, INT_MAX));
 
-        vector<vector<bool>> pacific(m, vector<bool>(n, false));
-        vector<vector<bool>> atlantic(m, vector<bool>(n, false));
+        // Pacific edges
+        int ocean = 1;
+        // top row
+        for(int i = 0; i < n; i++) {
+            if(grid[0][i] != ocean) dfs(0, i, ocean, grid, heights);
+        }
 
-        for(int i = 0; i < m; i++)  dfs(i, 0, pacific, heights);
-        for(int j = 0; j < n; j++)  dfs(0, j, pacific, heights);
+        // left col
+        for(int i = 0; i < m; i++) {
+            if(grid[i][0] != ocean) dfs(i, 0, ocean, grid, heights);
+        }
 
-        for(int i = 0; i < m; i++)  dfs(i, n - 1, atlantic, heights);
-        for(int j = 0; j < n; j++)  dfs(m - 1, j, atlantic, heights);
+        // Atlantic edges
+        ocean = -1;
+        // bottom row
+        for(int i = 0; i < n; i++) {
+            if(grid[m - 1][i] != ocean  && grid[m - 1][i] != 0) dfs(m - 1, i, ocean, grid, heights);
+        }
+
+        // right col
+        for(int i = 0; i < m; i++) {
+            if(grid[i][n - 1] != ocean && grid[i][n - 1] != 0) dfs(i, n - 1, ocean, grid, heights);
+        }
 
         vector<vector<int>> result;
-
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(pacific[i][j] && atlantic[i][j]) {
-                    result.push_back({i, j});
-                }
+                if(grid[i][j] == 0) result.push_back({i, j});
             }
         }
 
