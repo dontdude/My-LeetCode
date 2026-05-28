d5a3a70e9e45a0cf07c490bcf90832da28fdf072
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-28 10:04:31 +0530

    Difficulty: Medium, Time: (10 ms (71.27%)), Space: (14 MB (93.82%))

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,34 +1,18 @@
 class Solution {
 public:
-    // dp way
-    // int lengthOfLIS(vector<int>& nums) {
-    //     int n = nums.size(), res = 1;
-    //     vector<int> dp(n, 1);
-
-    //     for(int i = 1; i < n; i++) {
-    //         for(int j = i - 1; j >= 0; j--) {
-    //             if(nums[i] > nums[j]) dp[i] = max(dp[i], 1 + dp[j]);
-    //             res = max(res, dp[i];)
-    //         }
-    //     }
-
-    //     return res;
-    // }
-
-    // binary search way
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> incrSubseq;
+        vector<int> lis;
 
-        for(int& num : nums) {
-            if(incrSubseq.size() == 0 || incrSubseq.back() < num) {
-                incrSubseq.push_back(num);
+        for(const int& num : nums) {
+            if(lis.size() == 0 || lis.back() < num) {
+                lis.push_back(num);
                 continue;
             }
 
-            int ind = lower_bound(incrSubseq.begin(), incrSubseq.end(), num) - incrSubseq.begin();
-            incrSubseq[ind] = num;
+            int pos = lower_bound(lis.begin(), lis.end(), num) - lis.begin();
+            lis[pos] = num;
         }
 
-        return incrSubseq.size();
+        return lis.size();
     }
 };
