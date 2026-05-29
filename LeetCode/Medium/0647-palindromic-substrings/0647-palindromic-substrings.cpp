e9544ed47d25dd43d2bf4397094dad091514cc8e
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-05-29 09:12:09 +0530

    Difficulty: Medium, Time: (18 ms (42.51%)), Space: (34.7 MB (14.58%))

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    // recurrence relation
-    // rec(i, j)   // i = 0 -> n - 1    // j = n - 1 -> i
-    //     if(i > j) return false
-    //     if(i == j)  res++ return true
-    //
-    //     if(s[i] == s[j] && (j - i <= 2 || rec[i + 1][j - 1])) 
-    //          res++;
-    //          return true
-    //     
-    //     return false
-
     int countSubstrings(string s) {
         int n = s.size();
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int result = 0;
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        for(int i = n - 1; i >= 0; i--) {
-            for(int j = i; j < n; j++) { 
-                if(s[i] == s[j] && (j - i <= 2 || dp[i + 1][j - 1])) {  
-                    dp[i][j] = true;
-                    result++;
+        dp[n - 1][n - 1] = 1;
+        int count = n;   // all single char are already palindrome
+
+        for(int i = n - 2; i >= 0; i--) {
+            dp[i][i] = 1;
+            for(int j = i + 1; j < n; j++) {
+                if(s[i] == s[j]) {
+                    if(j - i <= 2 || dp[i + 1][j - 1] > 0) {  
+                        dp[i][j] = 1;
+                        count++;
+                    }
                 } 
             }
         }
 
-        return result;
+        return count;
     }
 };
