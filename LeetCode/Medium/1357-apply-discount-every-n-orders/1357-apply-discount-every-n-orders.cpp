f66853c6c21cf43c10334fe632a3fb5544a34e97
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-06-23 10:08:59 +0530

    Difficulty: Medium, Time: (172 ms (5.59%)), Space: (132 MB (53.42%))

---
+++ LeetCode/Medium/1357-apply-discount-every-n-orders/1357-apply-discount-every-n-orders.cpp
@@ -0,0 +1,40 @@
+class Cashier {
+    int customer;
+    int customerDiscountCycle;
+    int Discount;
+    unordered_map<int, int> priceMap;
+public:
+    Cashier(int n, int discount, vector<int>& products, vector<int>& prices) {
+        customerDiscountCycle = n;
+        customer = 0;
+        Discount = discount;
+
+        for(int i = 0; i < products.size(); i++) {
+            priceMap[products[i]] = prices[i];
+        }
+    }
+    
+    double getBill(vector<int> product, vector<int> amount) {
+        customer++;
+
+        double bill = 0;
+        for(int i = 0; i < product.size(); i++) {
+            bill += (amount[i] * priceMap[product[i]]);
+        }
+
+        if(customer == customerDiscountCycle) {
+            customer = 0;
+            
+            double discountedBill = (100.0 - Discount) * (bill / 100.0);
+            return discountedBill;
+        }
+
+        return bill;
+    }
+};
+
+/**
+ * Your Cashier object will be instantiated and called as such:
+ * Cashier* obj = new Cashier(n, discount, products, prices);
+ * double param_1 = obj->getBill(product,amount);
+ */
