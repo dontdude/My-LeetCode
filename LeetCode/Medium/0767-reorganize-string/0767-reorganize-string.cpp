211bec4b9c94d27c2496818c1355bba84d76e3ed
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-06-27 12:24:36 +0530

    Difficulty: Medium, Time: (3 ms (15.4%)), Space: (8.3 MB (92.42%))

--- LeetCode/Medium/0767-reorganize-string/0767-reorganize-string.cpp
+++ LeetCode/Medium/0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     string reorganizeString(string s) {
+        vector<int> freq(26, 0);
+        int mxFreq = 0;
         int n = s.size();
-        vector<int> count(26, 0);
-        
-        for(const auto& c: s) {
-            if(++count[c - 'a'] > (n + 1) / 2)  return "";
+
+        for(const char& c : s) {
+            mxFreq = max(mxFreq, ++freq[c - 'a']);
         }
+
+        if(mxFreq > (n + 1) / 2) return "";
 
         priority_queue<pair<int, char>> available;
-        pair<int, char> waiting = {-1, ' '};
+        pair<int, char> waiting = {-1, '#'};
 
         for(int i = 0; i < 26; i++) {
-            if(count[i]) available.push({count[i], 'a' + i});
+            if(freq[i])  available.push({freq[i], char('a' + i)});
         }
 
-        string res = "";
-        while(!available.empty() || waiting.first != -1) {
-            pair<int, char> newAvailable = {-1, ' '};
-            if(waiting.first != -1) {
-                newAvailable = waiting;
-                waiting = {-1, ' '};
-            }
-            
-            if(!available.empty()) {
-                res.push_back(available.top().second);
-                int newAvailableCount = available.top().first - 1;
-                if(newAvailableCount) waiting = {newAvailableCount, available.top().second};
-                available.pop();
+        string result = "";
+
+        while(!available.empty() || waiting.second != '#') {
+            pair<int, char> curr = available.top();
+            available.pop();
+
+            result.push_back(curr.second);
+
+            if(waiting.second != '#') {
+                available.push({waiting.first, waiting.second});
+                waiting = {-1, '#'};
             }
 
-            if(newAvailable.first != -1) {
-                available.push(newAvailable);
-            } 
+            if(curr.first > 1) waiting = {curr.first - 1, curr.second};
         }
 
-        return res;
+        return result;
     }
 };
