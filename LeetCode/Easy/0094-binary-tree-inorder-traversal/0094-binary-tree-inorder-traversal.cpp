4450bef1c623a26f7481248b45f422f6819460ff
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-06-30 08:37:43 +0530

    Difficulty: Easy, Time: (0 ms (100%)), Space: (13.4 MB (5.45%))

--- LeetCode/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ LeetCode/Easy/0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,71 +10,15 @@
  * };
  */
 class Solution {
+    vector<int> inorder;
 public:
-    // 1. Recursive Solution:
-    // vector<int> res;
-    // vector<int> inorderTraversal(TreeNode* root) {
-    //     if(root == NULL) return res;
-
-    //     inorderTraversal(root->left);   // traverse all the node in left, by breaking the control flow, to reach last
-    //     res.push_back(root->val);        
-    //     inorderTraversal(root->right);  // traverse all the node in right, by breaking the control flow
-
-    //     return res;
-    // }
-
-    // 2. Iterative Solution:
-    // vector<int> inorderTraversal(TreeNode* root) {
-    //     vector<int> res;
-    //     stack<TreeNode*> st;
-    //     TreeNode* curr = root;
-
-    //     while(curr || !st.empty()) {
-    //         while(curr) {               
-    //             st.push(curr);
-    //             curr = curr->left;      // traverse all the node in left, loop here breaks the control flow, to reach last
-    //         }
-
-    //         curr = st.top();   st.pop();
-    //         res.push_back(curr->val);
-
-    //         curr = curr->right;          // traverse all the node of right, auto breaks the control flow.. since last line
-    //     }
-
-    //     return res;
-    // }
-
-    // 3. Morris Inorder Traversal
-    TreeNode* findInorderPredessor(TreeNode* node) {
-        TreeNode* curr = node->left;
-        while(curr->right != NULL && curr->right != node) {
-            curr = curr->right;
-        }
-
-        return curr;
-    }
-
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> res;
-        TreeNode* curr = root;
+        if(root == nullptr) return inorder;
 
-        while(curr) {
-            if(curr->left == NULL) {
-                res.push_back(curr->val);
-                curr = curr->right;
-            } else {
-                TreeNode* pred = findInorderPredessor(curr);
-                if(pred->right == curr) {
-                    res.push_back(curr->val);
-                    curr = curr->right;
-                    pred->right = NULL;
-                } else {
-                    pred->right = curr;
-                    curr = curr->left;
-                }
-            }
-        }
+        inorderTraversal(root->left);
+        inorder.push_back(root->val);
+        inorderTraversal(root->right);
 
-        return res;
+        return inorder;
     }
 };
