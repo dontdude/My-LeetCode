3b887bbaa356ed69b633dbc0d5d4f77abce85b1c
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-07-06 10:41:34 +0530

    Difficulty: Medium, Time: (8 ms (80.57%)), Space: (10.5 MB (80.55%))

--- LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
+++ LeetCode/Medium/0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,21 +3,17 @@ public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
-        vector<uint64_t> prev(amount + 1, 0), curr(amount + 1, 0);
-        prev[0] = 1, curr[0] = 1;
+        vector<uint64_t> curr(amount + 1, 0);
+        curr[0] = 1;
 
         for(int i = 0; i < n; i++) {
             for(int j = 1; j <= amount; j++) {
-                curr[j] = 0;
                 if(coins[i] <= j) {
-                    curr[j] = curr[j - coins[i]];
+                    curr[j] += curr[j - coins[i]];
                 }
-
-                curr[j] += prev[j];
             }
-            prev = curr;
         }
 
-        return prev[amount];
+        return curr[amount];
     }
 };
