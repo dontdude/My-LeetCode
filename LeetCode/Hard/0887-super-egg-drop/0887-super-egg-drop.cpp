b812e020c126a992fbc11415dd948a0bccf5e014
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-07-10 22:04:21 +0530

    Difficulty: Hard, Time: (0 ms (100%)), Space: (8.1 MB (97.48%))

--- LeetCode/Hard/0887-super-egg-drop/0887-super-egg-drop.cpp
+++ LeetCode/Hard/0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
+    // int superEggDrop(int k, int n) {
+    //     vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));  // moves * eggs metrics. Where moves in worst case (eggs = 1) is equal to number of floor
+
+    //     int moves = 0;
+
+    //     while(dp[moves][k] < n) {
+    //         moves++;
+    //         for(int eggs = 1; eggs <= k; eggs++) {
+    //             int floors_possible_with_given_moves_and_broken_egg = dp[moves - 1][eggs - 1];
+    //             int floors_possible_with_given_moves_and_unbroken_egg = dp[moves - 1][eggs];
+
+    //             int current_floor = 1;
+
+    //             dp[moves][eggs] = current_floor 
+    //                                 + floors_possible_with_given_moves_and_broken_egg 
+    //                                 + floors_possible_with_given_moves_and_unbroken_egg;
+    //         }
+    //     }
+
+    //     return moves;
+    // }
+
+    // 1D approach
     int superEggDrop(int k, int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));  // moves * eggs metrics. Where moves in worst case (eggs = 1) is equal to number of floor
+        vector<int> dp(k + 1, 0);  
 
         int moves = 0;
 
-        while(dp[moves][k] < n) {
+        while(dp[k] < n) {
             moves++;
-            for(int eggs = 1; eggs <= k; eggs++) {
-                int floors_possible_with_given_moves_and_broken_egg = dp[moves - 1][eggs - 1];
-                int floors_possible_with_given_moves_and_unbroken_egg = dp[moves - 1][eggs];
-
-                int current_floor = 1;
-
-                dp[moves][eggs] = current_floor 
-                                    + floors_possible_with_given_moves_and_broken_egg 
-                                    + floors_possible_with_given_moves_and_unbroken_egg;
+            for(int eggs = k; eggs >= 1; eggs--) {
+                dp[eggs] += 1 + dp[eggs - 1];
             }
         }
 
