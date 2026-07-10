50cd9d97ebd3af26f74ae39cf895ea7ccc35e170
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-07-10 22:26:37 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (10.4 MB (95.38%))

--- LeetCode/Medium/0139-word-break/0139-word-break.cpp
+++ LeetCode/Medium/0139-word-break/0139-word-break.cpp
@@ -1,8 +1,10 @@
 class Solution {
-    bool isPatternMatching(int start, int size, string& s, string& word) {
-        if(start + size >  s.size()) return false;
-        for(int i = 0; i < size; i++) {
-            if(s[i + start] != word[i]) return false;
+    bool isWordMatch(string& s, int& i, string& word) {
+        int m = word.size(), n = s.size();
+
+        if(i + m > n) return false;
+        for(int j = 0; j < m; j++) {
+            if(s[i + j] != word[j])  return false;
         }
 
         return true;
@@ -11,21 +13,17 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
 
-        vector<char> dp(n + 1, '0');
-        dp[n] = '1';
+        vector<char> dp(n + 1, 0);
+        dp[n] = 1;
 
         for(int i = n - 1; i >= 0; i--) {
             for(string& word : wordDict) {
-                int size = word.size();
-                if((i + size) <= n && isPatternMatching(i, size, s, word)) {
-                    if(dp[i + size] == '1') { 
-                        dp[i] = '1';
-                        break;
-                    }
+                if(isWordMatch(s, i, word)) {
+                    dp[i] |= dp[i + word.size()];
                 }
             }
         }
 
-        return dp[0] == '1';
+        return dp[0];
     }
 };
