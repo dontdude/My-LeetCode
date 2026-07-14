48f660ef3071508b43c2ea10448fc814b9dca385
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-07-14 10:01:48 +0530

    Difficulty: Medium, Time: (0 ms (100%)), Space: (8.8 MB (46.87%))

--- LeetCode/Medium/0091-decode-ways/0091-decode-ways.cpp
+++ LeetCode/Medium/0091-decode-ways/0091-decode-ways.cpp
@@ -1,23 +1,33 @@
 class Solution {
-    bool isValidPair(const char& first, const char& second) {
-        return (first == '1') || (first == '2' && second < '7');
-    }
+    // Recurrence relation
+    // rec(i) i = n -> 1
+    //     if(i == 0 || i == 1)  rec[i] = 1
+
+    //     if(s[i - 1] != 0)  rec[i]  = rec[i - 1];
+
+    //     if(isValidPair(i - 1, i - 2))  {
+    //         rec[i] += rec[i - 2]
+    //     }
+
+    bool isValidPair(char curr, char prev) {
+        return (prev == '1' || (prev == '2' && curr <= '6'));
+    } 
 public:
     int numDecodings(string s) {
-        int n = s.size();
-        vector<int> dp(n, 0);
-        
         if(s[0] == '0')  return 0;
-        else dp[0] = 1;
 
-        for(int i = 1; i < n; i++) {
-            if(s[i] != '0')  dp[i] = dp[i - 1];
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1, dp[1] = 1;
 
-            if(isValidPair(s[i - 1], s[i])) {
-                dp[i] += (i > 1) ? dp[i - 2] : 1;   // 1 way of getting even empty 
+        for(int i = 2; i <= n; i++) {
+            if(s[i - 1] != '0')  dp[i] = dp[i - 1];
+
+            if(isValidPair(s[i - 1], s[i - 2])) {
+                dp[i] += dp[i - 2];
             }
         }
 
-        return dp[n - 1];
+        return dp[n];
     }
 };
