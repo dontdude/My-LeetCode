440beae7067c60fe0639c502ec5b2ebb9eb8c7ed
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-07-17 11:05:09 +0530

    Difficulty: Medium, Time: (15 ms (45.87%)), Space: (16 MB (35.04%))

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.size();
-        
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<vector<char>> dp(n, vector<char>(n, 0));
 
-        dp[n - 1][n - 1] = 1;
-        int count = n;   // all single char are already palindrome
+        int palindromes = 0;
 
-        for(int i = n - 2; i >= 0; i--) {
+        for(int i = n - 1; i >= 0; i--) {
             dp[i][i] = 1;
+            palindromes++;
+
             for(int j = i + 1; j < n; j++) {
-                if(s[i] == s[j]) {
-                    if(j - i <= 2 || dp[i + 1][j - 1] > 0) {  
-                        dp[i][j] = 1;
-                        count++;
-                    }
-                } 
+                if(s[i] == s[j] && ((i + 1 == j) || dp[i + 1][j - 1])) {
+                    dp[i][j] = 1;
+                    palindromes++;
+                }   
             }
         }
 
-        return count;
+        return palindromes;
     }
 };
