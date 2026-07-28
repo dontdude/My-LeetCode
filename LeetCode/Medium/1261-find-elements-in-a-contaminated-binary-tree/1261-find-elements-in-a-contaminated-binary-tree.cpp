2454508577469aa76c60ccbb2446399d467d451f
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-07-28 10:54:54 +0530

    Difficulty: Medium, Time: (3 ms (96.45%)), Space: (26.7 MB (81.95%))

--- LeetCode/Medium/1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
+++ LeetCode/Medium/1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -9,27 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class FindElements { 
-    // why recovering can work, even though level of tree is huge 20, meaning a lot of nodes in full tree, but there is also a constraints that limits number of nodes in tree to 10^4
-    void recoverTree(TreeNode* node, int x) {
-        if(node == nullptr)  return;
-
-        node->val = x;
-        hashNode.insert(x);
-
-        if(node->left)  recoverTree(node->left, 2 * x + 1);
-        if(node->right) recoverTree(node->right, 2 * x + 2);
-    }
-    TreeNode* rootNode;
-    unordered_set<int> hashNode;
+class FindElements {
+    vector<int> nodeVals;
 public:
     FindElements(TreeNode* root) {
-        rootNode = root;
-        recoverTree(root, 0);
+        if(root == nullptr) return;
+
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        nodeVals.push_back(0);
+
+        while(!q.empty()) {
+            TreeNode* node = q.front().first;
+            int x = q.front().second;
+            q.pop();
+
+            if(node->left) {
+                int leftNodeVal = 2 * x + 1;
+                nodeVals.push_back(leftNodeVal);
+                q.push({node->left, leftNodeVal});
+            }
+
+            if(node->right) {
+                int rightNodeVal = 2 * x + 2;
+                nodeVals.push_back(rightNodeVal);
+                q.push({node->right, rightNodeVal});
+            }
+        }
     }
     
     bool find(int target) {
-        return hashNode.find(target) != hashNode.end();
+        auto it = lower_bound(nodeVals.begin(), nodeVals.end(), target);
+        return it != nodeVals.end() && *it == target;
     }
 };
 
