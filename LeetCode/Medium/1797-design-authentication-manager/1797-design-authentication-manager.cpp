061322ae7156c1a4608bfe4b45250207ef67b715
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-07-30 17:17:40 +0530

    Difficulty: Medium, Time: (17 ms (51.57%)), Space: (43.3 MB (6.08%))

---
+++ LeetCode/Medium/1797-design-authentication-manager/1797-design-authentication-manager.cpp
@@ -0,0 +1,52 @@
+class AuthenticationManager {
+    int ttl;
+    unordered_map<string, int> tokenHash;
+    map<int, unordered_set<string>> timeMap;
+
+    void removeExpiredTokens(int& currentTime) {
+        while(timeMap.size()) {
+            int minExpiryTime = timeMap.begin()->first;
+            if(minExpiryTime > currentTime)  return;
+
+            for(const string& token : timeMap[minExpiryTime]) {
+                tokenHash.erase(token);
+            }
+            timeMap.erase(minExpiryTime);
+        } 
+    }
+public:
+    AuthenticationManager(int timeToLive) {
+        ttl = timeToLive;
+    }
+    
+    void generate(string tokenId, int currentTime) {
+        int expiryTime = currentTime + ttl;
+        tokenHash[tokenId] = expiryTime;
+        timeMap[expiryTime].insert(tokenId);
+    }
+    
+    void renew(string tokenId, int currentTime) {
+        removeExpiredTokens(currentTime);
+
+        if(tokenHash.find(tokenId) == tokenHash.end())  return;
+
+        int oldExpiryTime = tokenHash[tokenId];
+        timeMap[oldExpiryTime].erase(tokenId);
+
+        generate(tokenId, currentTime);
+    }
+    
+    int countUnexpiredTokens(int currentTime) {
+        removeExpiredTokens(currentTime);
+
+        return tokenHash.size();
+    }
+};
+
+/**
+ * Your AuthenticationManager object will be instantiated and called as such:
+ * AuthenticationManager* obj = new AuthenticationManager(timeToLive);
+ * obj->generate(tokenId,currentTime);
+ * obj->renew(tokenId,currentTime);
+ * int param_3 = obj->countUnexpiredTokens(currentTime);
+ */
