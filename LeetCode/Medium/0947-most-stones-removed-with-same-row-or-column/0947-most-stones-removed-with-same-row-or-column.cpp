6fe07bccf4805ad062a588241f1a276291b0da5c
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-08-18 00:23:19 +0530

    Difficulty: Medium, Time: (31 ms (42.71%)), Space: (58.5 MB (5.01%))

---
+++ LeetCode/Medium/0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    int n;
+    void dfs(int stone, int row, int col, vector<char>& vis, vector<vector<pair<int, int>>>& rs, vector<vector<pair<int, int>>>& cs) {
+        vis[stone] = 1;
+
+        for(const auto& cells : rs[row]) {
+            if(vis[cells.first] == 1) continue;
+
+            int rowStone = cells.first;
+            int newCol = cells.second;
+            dfs(rowStone, row, newCol, vis, rs, cs);
+        }
+
+        for(const auto& cells : cs[col]) {
+            if(vis[cells.first] == 1)  continue;
+
+            int colStone = cells.first;
+            int newRow = cells.second;
+            dfs(colStone, newRow, col, vis, rs, cs);
+        }
+    }
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        n = stones.size();
+        vector<char> vis(n, 0);
+
+        vector<vector<pair<int, int>>> rowStones(1e4 + 1);
+        vector<vector<pair<int, int>>> colStones(1e4 + 1);
+
+        for(int i = 0; i < n; i++) {
+            rowStones[stones[i][0]].push_back({i, stones[i][1]});
+            colStones[stones[i][1]].push_back({i, stones[i][0]});
+        }
+
+        int connectedComponents = 0;
+        for(int i = 0; i < n; i++) {
+            if(vis[i] == 0) {
+                connectedComponents++;
+                dfs(i, stones[i][0], stones[i][1], vis, rowStones, colStones);
+            }
+        }
+
+        return n - connectedComponents;
+    }
+};
