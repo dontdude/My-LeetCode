44c85e12ed5ab6edf94feca49730d68b7030c6ef
Chandan Mishra <75321407+dontdude@users.noreply.github.com>
2026-08-23 10:58:41 +0530

    Difficulty: Medium, Time: (4 ms (46.22%)), Space: (18.1 MB (50.88%))

--- LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    // using bellman ford: each iteration i, is basically the number of edges used now to get new dist
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        int  edgesRequiredForKStops = k + 1;
-        
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
+        vector<vector<pair<int, int>>> graph(n, vector<pair<int, int>>());
 
-        for(int i = 0; i < edgesRequiredForKStops; i++) {
-
-            // prevents taking multiple flights in a single step (if i = 1, then calc for only flight using 1 edge)
-            vector<int> temp = dist;
-
-            for(const auto&flight : flights) {
-                int u = flight[0];
-                int v = flight[1];
-                int currDist = flight[2];
-                
-                if(dist[u] != INT_MAX && temp[v] > dist[u] + currDist) {
-                    temp[v] = dist[u] + currDist;
-                }
-            }
-
-            dist = temp;
+        for(const auto& flight : flights) {
+            graph[flight[0]].push_back({flight[1], flight[2]});
         }
 
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        vector<int> prices(n, INT_MAX);
+        queue<pair<int, int>> q;
+
+        q.push({src, 0});
+        prices[src] = 0;
+        int level = 0;
+
+        while(!q.empty() && level <= k + 1) {
+            int s = q.size();
+            level++;
+
+            while(s--) {
+                int u = q.front().first;
+                int u_reachable_pirce = q.front().second;
+                q.pop();
+
+                prices[u] = min(u_reachable_pirce, prices[u]);
+                if(prices[u] >= prices[dst]) continue;
+
+                for(const auto& nebr : graph[u]) {
+                    int v = nebr.first;
+                    int price = nebr.second;
+
+                    if(prices[v] > u_reachable_pirce + price) {
+                        q.push({v, u_reachable_pirce + price});
+                    }
+                }
+            }
+        }
+
+        return prices[dst] == INT_MAX ? -1 : prices[dst];
     }
 };
